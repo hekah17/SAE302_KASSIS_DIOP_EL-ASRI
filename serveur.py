a1b8503f98e9deb4c301a4e173ed49d61dd66e33
endel <k6.sarah@gmail.com>
2025-12-18 12:57:16 +0100

    erreur server resolve

--- serveur.py
+++ serveur.py
@@ -1,1 +1,12 @@
 import json
+from flask import Flask, render_template  #on ajoute flask et le repertoire templates qui va servir au html
+
+app = Flask(__name__) #variable de l'app web contenant une instance de flask
+
+@app.route("/") #quand quelqu'un cherche l'adresse racine du site, il tombre sur la fonction suivante (normal pour un index d'avoir la racine)
+def home(): 
+    return render_template("index.html")    #flasl accède à templates/index.html pour l'envoyé au navigateur du client
+
+#sécurité/debugg
+if __name__ == "__main__":
+    app.run(debug=True) #live server pour afficher la page à chaque modif 
