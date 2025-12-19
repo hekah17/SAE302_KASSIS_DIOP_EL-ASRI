52d1ee561bd067a3245282cd359eac7dd346507a
hekah17 <kassis.heloise@gmail.com>
2025-12-19 23:30:31 +0100

    19 12 25

--- serveur.py
+++ serveur.py
@@ -1,12 +1,18 @@
-import json
-from flask import Flask, render_template  #on ajoute flask et le repertoire templates qui va servir au html
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
-app = Flask(__name__) #variable de l'app web contenant une instance de flask
+app = Flask(__name__) #création de l'application __name__
+app.config['SECRET_KEY'] = 'secret!' #mesure de sécurité de flask
+socketio = SocketIO(app) #on greffe notre application au socket (permet le instantané)
 
-@app.route("/") #quand quelqu'un cherche l'adresse racine du site, il tombre sur la fonction suivante (normal pour un index d'avoir la racine)
-def home(): 
-    return render_template("index.html")    #flasl accède à templates/index.html pour l'envoyé au navigateur du client
+@app.route('/') #quand on tape l'adresse du site à sa racine
+def index():
+    return render_template('chat.html') #on retourne la page chat.html, dans le dossier templates
 
-#sécurité/debugg
-if __name__ == "__main__":
-    app.run(debug=True) #live server pour afficher la page à chaque modif 
+@socketio.on('message') #on met le serveur en écoute du moindre evenement
+def handle_message(msg): #msg=le message envoyé
+    print('Message reçu du client: ' + msg) #le print est dans le terminale, c'est orienté dev
+    send(msg, broadcast=True) #envoie le message en broadcast
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)#on lance l'application avec le socket pour le coté instantané, avec debug en on pour avoir les log d'erreur
